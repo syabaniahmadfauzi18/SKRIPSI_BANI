bd2e4c5cfe43a77db11a093aa03296dc302d8250
Sya'bani Ahmad Fauzi <baniaf67@gmail.com>
2022-12-30 14:17:50 +0700

    Inisialisasi Project

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong> &copy; 2019 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE. | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+	
+</footer>
